bebace603d210c9072a379e03845ceb67774a673
ltbliu <cloud81115@hotmail.com.tw>
2018-08-03 08:22:22 +0800

    add pagination

--- nmyproject/boards/models.py
+++ nmyproject/boards/models.py
@@ -8,12 +8,17 @@ class Board(models.Model):
 	description = models.CharField(max_length=100)
 	def __str__(self):
 		return self.name
+	def get_posts_count(self):
+		return Post.objects.filter(topic__board=self).count()
+	def get_last_post(self):
+		return Post.objects.filter(topic__board=self).order_by('-created_at').first()
 
 class Topic(models.Model):
 	subject = models.CharField(max_length=255)
 	last_updated = models.DateTimeField(auto_now_add=True)
 	board = models.ForeignKey(Board, related_name='topics', on_delete=models.CASCADE)
 	starter = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE)
+	views = models.PositiveIntegerField(default=0)
 	def __str__(self):
 		return self.subject
 class Post(models.Model):
--- nmyproject/boards/views.py
+++ nmyproject/boards/views.py
@@ -1,16 +1,31 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render,get_object_or_404,redirect
+from django.views.generic import UpdateView, ListView
+from django.utils import timezone
 from .models import Board, Topic, Post
 from .forms import NewTopicForm, PostForm
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.db.models import Count
+from django.core.paginator import Paginator
 # Create your views here.
-def home(request):
+"""def home(request):
 	boards = Board.objects.all()
-	return render(request,'home.html',{'boards':boards})
+	return render(request,'home.html',{'boards':boards})"""
+class BoardListView(ListView):
+	model = Board
+	context_object_name = 'boards'
+	template_name = 'home.html'
 
 def board_topics(request, pk):
 	board = get_object_or_404(Board,pk=pk)
-	return render(request, 'topics.html', {'board':board})
+	queryset = board.topics.order_by('-last_updated').annotate(replies=Count('posts')-1)
+	page_num = request.GET.get('page',1)
+	paginator = Paginator(queryset ,10)
+	#page= paginator.get_page(page_num)
+	topics = paginator.get_page(page_num)
+
+	return render(request, 'topics.html', {'board':board,'topics':topics})
 # raw html form
 """def new_topic(request, pk):
 	board = get_object_or_404(Board, pk=pk)
@@ -54,7 +69,17 @@ def new_topic(request, pk):
 
 def topic_posts(request, pk, topic_pk):
 	topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
-	return render(request, 'topic_posts.html', {'topic': topic})
+	topic.views+=1
+	topic.save()
+	queryset = topic.posts.order_by('-created_at','id')
+	#print(queryset)
+	paginator = Paginator(queryset ,2)
+	print(paginator)
+	page_num = request.GET.get('page',1)
+	post_reply = paginator.get_page(page_num)
+	print(post_reply)
+	return render(request, 'topic_posts.html', {'topic': topic,'post_reply':post_reply})
+
 @login_required
 def reply_topic(request, pk, topic_pk):
 	topic =get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
@@ -69,3 +94,21 @@ def reply_topic(request, pk, topic_pk):
 	else:
 		form = PostForm()
 	return render(request, 'reply_topic.html', {'topic':topic, 'form':form })
+
+@method_decorator(login_required, name='dispatch')
+class PostUpdateView(UpdateView):
+	model = Post
+	fields = ('message',)
+	template_name = 'edit_post.html'
+	pk_url_kwarg = 'post_pk'
+	context_object_name = 'post'
+	def get_queryset(self):
+		queryset = super().get_queryset()
+		return queryset.filter(created_by=self.request.user)
+
+	def form_valid(self,form):
+		post = form.save(commit=False)
+		post.updated_by = self.request.user
+		post.updated_at = timezone.now()
+		post.save()
+		return redirect('topic_post', pk=post.topic.board.pk, topic_pk=post.topic.pk)
